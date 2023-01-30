e51e329ea94b2e123ecc58da67dd340c4e86f2d2
Jerome Garec <jerome.garec@gmail.com>
2023-01-30 17:19:57 +0100

    fixes #140: allow to run liquibase history command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ if [[ "$INSTALL_MYSQL" ]]; then
   lpm add mysql --global
 fi
 
-if type "$1" > /dev/null 2>&1; then
-  ## First argument is an actual OS command. Run it
+if [[ "$1" != "history" ]] && type "$1" > /dev/null 2>&1; then
+  ## First argument is an actual OS command (except if the command is history as it is a liquibase command). Run it
   exec "$@"
 else
   if [[ "$*" == *--defaultsFile* ]] || [[ "$*" == *--defaults-file* ]] || [[ "$*" == *--version* ]]; then
