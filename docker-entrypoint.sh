80b9eb11cefef8b599c2eeacb2f12ffa428a5ab3
Sayali Mohadikar <76010603+sayaliM0412@users.noreply.github.com>
2024-02-15 09:50:37 -0600

    DAT-16879: explicitly adding liquibase initialization command for H2 (#291)
    * explicitly adding liquibase initialization command for H2
    
    * explicitly adding liquibase initialization command for H2
    
    * chore(docker-entrypoint.sh): update comment to include "init" command as a liquibase command
    
    ---------
    
    Co-authored-by: Sayali M <sayali@Sayalis-MacBook-Pro>
    Co-authored-by: jandroav <jandroav@icloud.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ if [[ "$INSTALL_MYSQL" ]]; then
   lpm add mysql --global
 fi
 
-if [[ "$1" != "history" ]] && type "$1" > /dev/null 2>&1; then
-  ## First argument is an actual OS command (except if the command is history as it is a liquibase command). Run it
+if [[ "$1" != "history" ]] && [[ "$1" != "init" ]] && type "$1" > /dev/null 2>&1; then
+  ## First argument is an actual OS command (except if the command is history or init as it is a liquibase command). Run it
   exec "$@"
 else
   if [[ "$*" == *--defaultsFile* ]] || [[ "$*" == *--defaults-file* ]] || [[ "$*" == *--version* ]]; then
@@ -17,4 +17,3 @@ else
     exec /liquibase/liquibase "--defaultsFile=/liquibase/liquibase.docker.properties" "$@"
   fi
 fi
-
