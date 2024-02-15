c21ccec6e8ca34e58f73a61d97eb1a1ff3708b97
jandroav <jandroav@icloud.com>
2024-02-15 10:27:02 +0100

    chore(docker-entrypoint.sh): update comment to include "init" command as a liquibase command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [[ "$INSTALL_MYSQL" ]]; then
 fi
 
 if [[ "$1" != "history" ]] && [[ "$1" != "init" ]] && type "$1" > /dev/null 2>&1; then
-  ## First argument is an actual OS command (except if the command is history as it is a liquibase command). Run it
+  ## First argument is an actual OS command (except if the command is history or init as it is a liquibase command). Run it
   exec "$@"
 else
   if [[ "$*" == *--defaultsFile* ]] || [[ "$*" == *--defaults-file* ]] || [[ "$*" == *--version* ]]; then
